22b7065187454a8ec9c6ac7b3010b237599cc168
ODUZZ <tobiloba428@gmail.com>
2026-08-06 17:24:29 +0100

    feat: initialize ODUZZ OS

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>ODUZZ OS</h1>
+      <p>Welcome to the future of intelligent electrical management.</p>
+    </div>
+  );
+}
+
+export default App;
